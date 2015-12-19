d5051c9970143b3f980fc6b18ec27637b24c7887
Swetha24 <swetharaghukrishnan@gmail.com>
2015-12-19 21:21:58 +0530

    Update modify3.php

--- modify3.php
+++ modify3.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Control Panel :: Student Information Panel:: Institute of Information Technology, BZU</title>
+<title>Control Panel :: Student Information Panel</title>
 <style type="text/css">
 <!--
 .heading {
